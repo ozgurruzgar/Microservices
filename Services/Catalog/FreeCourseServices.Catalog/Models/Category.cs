c41604f9045372ac016a1ef649926913a3b23ebb
Özgür Rüzgar <94163712+ozgurruzgar@users.noreply.github.com>
2023-07-27 00:53:24 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourseServices.Catalog/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourseServices.Catalog.Models
+{
+    public class Category
+    {
+        [BsonId] //This statement as a real Id value provides by MongoDB
+        [BsonRepresentation(BsonType.ObjectId)] // This statement provides give type the prop.
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
